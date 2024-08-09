2301e74aa43fd4a060aba0e489f029c298585567
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-09 16:40:22 -0300

    readmore post section

---
+++ src/components/posts/sections/ReadmorePostSection/index.tsx
@@ -0,0 +1,10 @@
+import { Title } from "../../../shared/fragments/content/Title";
+
+export function ReadmorePostSection() {
+  return (
+    <section>
+      <Title tag="h2">Leia também</Title>
+      {/* PostList */}
+    </section>
+  );
+}
--- src/pages/SinglePostPage/index.tsx
+++ src/pages/SinglePostPage/index.tsx
@@ -1,11 +1,13 @@
 import { PostCommentsSection } from "../../components/comments/sections/PostCommentsSection";
 import { PostContentSection } from "../../components/posts/sections/PostContentSection";
+import { ReadmorePostSection } from "../../components/posts/sections/ReadmorePostSection";
 
 export function SinglePostPage() {
   return (
     <>
       <PostContentSection />
       <PostCommentsSection />
+      <ReadmorePostSection />
     </>
   );
 }
