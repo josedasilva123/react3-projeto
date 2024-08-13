c3559cd2d1f102bf42f316478ceb15ebb3705c41
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-13 19:16:33 -0300

    title styles part1

--- src/components/shared/fragments/content/Title/index.tsx
+++ src/components/shared/fragments/content/Title/index.tsx
@@ -2,18 +2,46 @@ import { HTMLAttributes } from "react";
 
 export type TitleTags = "h1" | "h2" | "h3" | "h4";
 
-interface Props extends HTMLAttributes<HTMLElement> {
+interface Props extends Omit<HTMLAttributes<HTMLElement>, "className"> {
   children: React.ReactNode;
   tag: TitleTags;
+  size: "one" | "two" | "three" | "four";
+  titleStyle: "primary" | "secondary";
+  italic: boolean;
 }
 
-export function Title({ children, tag, ...rest }: Props) {
+export function Title({
+  children,
+  tag,
+  size,
+  titleStyle,
+  italic = false,
+  ...rest
+}: Props) {
+  const titleClassName = `title ${size} ${titleStyle} ${italic ? "italic" : ""}`;
+
   return (
     <>
-      {tag === "h1" ? <h1 {...rest}>{children}</h1> : null}
-      {tag === "h2" ? <h2 {...rest}>{children}</h2> : null}
-      {tag === "h3" ? <h3 {...rest}>{children}</h3> : null}
-      {tag === "h4" ? <h4 {...rest}>{children}</h4> : null}
+      {tag === "h1" ? (
+        <h1 {...rest} className={titleClassName}>
+          {children}
+        </h1>
+      ) : null}
+      {tag === "h2" ? (
+        <h2 {...rest} className={titleClassName}>
+          {children}
+        </h2>
+      ) : null}
+      {tag === "h3" ? (
+        <h3 {...rest} className={titleClassName}>
+          {children}
+        </h3>
+      ) : null}
+      {tag === "h4" ? (
+        <h4 {...rest} className={titleClassName}>
+          {children}
+        </h4>
+      ) : null}
     </>
   );
 }
