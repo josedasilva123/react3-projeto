181b2f536d0db13e4b62a689e291adcbd22ce24f
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-14 17:28:13 -0300

    text styles part2

--- src/components/shared/fragments/content/Text/index.tsx
+++ src/components/shared/fragments/content/Text/index.tsx
@@ -1,15 +1,27 @@
 import { HTMLAttributes } from "react";
 
-interface Props extends HTMLAttributes<HTMLElement> {
+interface Props extends Omit<HTMLAttributes<HTMLElement>, "className"> {
   children: React.ReactNode;
+  size: "default" | "md" | "sm";
+  opacity?: boolean;
   tag: "p" | "span";
 }
 
-export function Text({ children, tag, ...rest }: Props) {
+export function Text({ children, size, opacity, tag, ...rest }: Props) {
+  const textClassName = `text ${size} ${opacity ? "opacity" : ""}`;
+
   return (
     <>
-      {tag === "p" ? <p {...rest}>{children}</p> : null}
-      {tag === "span" ? <span {...rest}>{children}</span> : null}
+      {tag === "p" ? (
+        <p {...rest} className={textClassName}>
+          {children}
+        </p>
+      ) : null}
+      {tag === "span" ? (
+        <span {...rest} className={textClassName}>
+          {children}
+        </span>
+      ) : null}
     </>
   );
 }
