cf3620a0b22d5a08f5e891da1842ea646056a064
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-18 18:18:17 -0300

    highlights styles part1

--- src/components/posts/sections/HighlightPostSection/PostCard/index.tsx
+++ src/components/posts/sections/HighlightPostSection/PostCard/index.tsx
@@ -3,6 +3,7 @@ import { IPost } from "../../../../../interfaces/posts.interface";
 import { Title } from "../../../../shared/fragments/content/Title";
 import { Button } from "../../../../shared/fragments/buttons/Button";
 import { CategoryName } from "../../../../categories/fragments/CategoryName";
+import styles from "./style.module.scss";
 
 interface Props {
   post: IPost;
@@ -10,14 +11,20 @@ interface Props {
 
 export function PostCard({ post }: Props) {
   return (
-    <li>
+    <li className={styles.card}>
       <Link to={`/post/${post.id}`}>
-        <img src={post.image} alt={`Ilustração do post ${post.title}`} />
-        <div>
+        {post.image ? (
+          <img className={styles.image} src={post.image} alt={`Ilustração do post ${post.title}`} />
+        ) : null}
+        <div className={styles.box}>
           <CategoryName categoryId={post.categoryId} />
-          <Title tag="h3" size="three" titleStyle="secondary">{post.title}</Title>
+          <Title tag="h3" size="four" titleStyle="secondary">
+            {post.title}
+          </Title>
         </div>
-        <Button buttonColor="solid2" size="round">-&gt;</Button>
+        <Button buttonColor="solid2" size="round" title="Ler mais">
+          <span className="material-symbols-outlined">arrow_forward</span>
+        </Button>
       </Link>
     </li>
   );
