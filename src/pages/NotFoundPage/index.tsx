17990b025ecfd3f760f4e885d94285cf1b52b55e
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-20 17:38:33 -0300

    404 page styles

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,7 +1,19 @@
-export function NotFoundPage(){
-    return(
-        <>
-            <h1>Página não encontrada</h1>
-        </>
-    )
+import { ButtonLink } from "../../components/shared/fragments/buttons/ButtonLink";
+import { Title } from "../../components/shared/fragments/content/Title";
+import { Container } from "../../components/shared/fragments/grid/Container";
+import styles from "./style.module.scss"
+
+export function NotFoundPage() {
+  return (
+    <Container>
+      <div className={styles.box}>
+        <Title tag="h1" size="one" titleStyle="primary">
+          Página não encontrada
+        </Title>
+        <ButtonLink buttonColor="outline" to="/">
+          Voltar para home
+        </ButtonLink>
+      </div>
+    </Container>
+  );
 }
